d68a60ce6ad6dd056acd8ec399c972ee021f12cf
Aschalew <aschalew.tesfaye15@gmail.com>
2025-09-14 13:37:59 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const btn = document.getElementById('hover_btn');
+const box = document.getElementById("container")
+
+btn.addEventListener('click', () => {   
+  
+  box.classList.toggle('active');
+});
+
+//
+
+
+
